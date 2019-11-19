2e0a6a064cd8a1f2d8efb620f1ee2b60508fc2bb
MondaleFelix <mondalefelix@yahoo.com>
2019-11-19 14:24:06 -0800

    added function to the button

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        print("This is coming from the shake button")
+    }
+    
 }
 
