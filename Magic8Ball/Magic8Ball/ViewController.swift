3fcdf4ead433140d5b3779895ab62b043f78752f
MondaleFelix <mondalefelix@yahoo.com>
2019-11-19 14:27:56 -0800

    button pressed changes label to fortune

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
     @IBOutlet weak var answerLabel: UILabel!
     
     @IBOutlet weak var shakeButton: UIButton!
@@ -19,7 +20,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func shakeButtonTapped(_ sender: Any) {
-        print("This is coming from the shake button")
+        let randomIndex = Int.random(in: 0..<answers.count)
+        answerLabel.text = answers[randomIndex]
     }
     
 }
